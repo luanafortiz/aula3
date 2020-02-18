a4f54d3f91632828d255efb9033a31c9c7d29734
Luana <luanadfortiz@gmail.com>
2020-02-18 17:00:10 -0300

    falta ver se é float

---
+++ exercicio5.py
@@ -0,0 +1,6 @@
+import math
+raio = float (input ('digite um raio: '))
+
+area = math.pi * raio**2
+comprimento = 2 * (math.pi * raio)
+print ('uma circunferencia de raio',raio, 'tem uma área de', area, 'e um comprimento de',comprimento)
