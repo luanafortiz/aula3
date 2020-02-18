d057a2c3f84c0ab0d220caf8f52d99e66e606688
Luana <luanadfortiz@gmail.com>
2020-02-18 16:16:42 -0300

    feito

---
+++ exercicio4.py
@@ -0,0 +1,8 @@
+x = 1
+caracter = input('digite um caracter: ')
+caracter1 = caracter
+degrau = int(input ('digite a quantidade de degraus desejada: '))
+while x <= degrau:
+    print(caracter1)
+    caracter1 = caracter1 + caracter
+    x += 1
