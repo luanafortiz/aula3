bf850e7145dc30b5a161547b4979c6caa03e18d6
Luana <luanadfortiz@gmail.com>
2020-02-20 15:29:23 -0300

    fiz funcoes

--- exercicio5.py
+++ exercicio5.py
@@ -1,6 +1,12 @@
 import math
 raio = float (input ('digite um raio: '))
 
-area = math.pi * raio**2
-comprimento = 2 * (math.pi * raio)
-print ('uma circunferencia de raio',raio, 'tem uma área de', area, 'e um comprimento de',comprimento)
+def area (raio):
+    area = math.pi * raio**2
+    return (area)
+def comprimento (raio):
+    comprimento = 2 * (math.pi * raio)
+    return(comprimento)
+areaa = area(raio)
+comprimentoo = comprimento(raio)
+print ('uma circunferencia de raio',raio, 'tem uma área de', areaa, 'e um comprimento de',comprimentoo)
